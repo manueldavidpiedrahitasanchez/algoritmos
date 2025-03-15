d576b9f5cc465b94d3ce8c37c97ace16aca89f89
¨manueldavidpiedrahitasanchez¨ <mdpiedrahita07@gmail.com>
2025-03-14 19:28:36 -0500

    introduccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,18 @@
+
+# autor: david
+# este es un comentario de una sola linea
+''
+print ("Estudiando python")
+
+#Definicion de variablesN
+
+entrada_de_datos
+
+reglas 
+python es sensible a minusculas y mayusculas  # type: ignore
+
+no se pueden usar espacios
+x=50
+y=15
+
+print 
