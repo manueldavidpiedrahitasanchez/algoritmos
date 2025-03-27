bb947c78d75c668f40b5ec8799b919765e06c098
¨manueldavidpiedrahitasanchez¨ <mdpiedrahita07@gmail.com>
2025-03-26 20:06:34 -0500

    ejercicio_4

--- ejercicios_de_repaso/ejercicio4.py
+++ ejercicios_de_repaso/ejercicio4.py
@@ -2,3 +2,7 @@
 Problema: Escribe un programa que determine si un número ingresado por el 
 usuario es par o impar utilizando el operador módulo %.
 '''
+
+y=float(input("ingrese un numero"))
+
+print(y%2==0)
