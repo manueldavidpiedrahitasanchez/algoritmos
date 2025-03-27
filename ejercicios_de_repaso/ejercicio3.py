35f893c2dca7f0df38fb72d0f48789c7f7bb2324
¨manueldavidpiedrahitasanchez¨ <mdpiedrahita07@gmail.com>
2025-03-26 19:49:36 -0500

    ejercicio_3

--- ejercicios_de_repaso/ejercicio3.py
+++ ejercicios_de_repaso/ejercicio3.py
@@ -2,3 +2,9 @@
 Problema: Escribe un programa que solicite un número al usuario y lo incremente en 5 unidades y 
 luego lo decremente en 3.
 '''
+
+y=float(input("ingrese numero"))
+y+=5
+y-=3
+
+print(y)
